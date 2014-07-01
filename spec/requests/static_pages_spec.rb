e00c2213787f756c118c0b077f3d8ca00b8e7336
Michael T. Minogue <michael.minogue@gmail.com>
2014-07-01 08:00:03 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+  	
+  	before { visit root_path }
+  	let(:heading)    { 'ProfitCruncher' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+end
